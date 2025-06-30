eb7d8ae2da6ee2c63d04e51e043985f2561025c5
Adithya N T <adithyant982@gmail.com>
2025-06-30 22:32:10 +0530

    Delete sample_success_project directory

--- sample_success_project/test_app.py
+++
@@ -1,6 +0,0 @@
-"""Test for add function in app module."""
-from sample_success_project.app import add
-
-def test_add():
-    """Test add function with simple values."""
-    assert add(2, 3) == 5 
